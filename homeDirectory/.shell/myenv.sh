51dfed2df8226925ec07c8a815145c5a7e224370
TylerWang <tyler@Tylers-MBP.lan>
2017-04-19 23:01:42 +0800

    add maven

--- homeDirectory/.shell/myenv.sh
+++ homeDirectory/.shell/myenv.sh
@@ -1,13 +1,19 @@
 
+export srcapp=/usr/local/srcapp
+
 #export GRADLE_HOME=/Users/tyler/Documents/java/gradle2.13
 #export PATH=$PATH:$GRADLE_HOME/bin
-#export M2_HOME=/Users/tyler/.m2
-#export M3_HOME=/Users/tyler/.m2
-#export PATH=$PATH:$M3_HOME
+
+export MAVEN_HOME=$srcapp/apache-maven-3.5.0/bin
 
 # go env
 export GOPATH=/Users/tyler/code/goWorkspaces
-export PATH=$PATH:$GOPATH/bin
+#export PATH=$PATH:$GOPATH/bin
 
 
+envArray=( $MAVEN_HOME  )
+for i in "${envArray[@]}"
+do
+	export PATH=$PATH:$i
+done
 
--- homeDirectory/install.sh
+++ homeDirectory/install.sh
@@ -2,3 +2,9 @@ ln .zshrc ~/.zshrc
 ln .vimrc ~/.vimrc
 ln .shell/myalias.sh ~/.shell/myalias.sh
 ln .shell/myenv.sh ~/.shell/myenv.sh
+
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
