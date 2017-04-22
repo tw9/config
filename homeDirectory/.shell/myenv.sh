b107e9b76e84d3be3a43b075e7776e8779c4bc09
TylerWang <tyler@Tylers-MBP.lan>
2017-04-22 14:43:01 +0800

    modfiy gopath

--- homeDirectory/.shell/myenv.sh
+++ homeDirectory/.shell/myenv.sh
@@ -1,15 +1,16 @@
 
 export srcapp=/usr/local/srcapp
 
-
 export MAVEN_HOME=$srcapp/apache-maven-3.5.0
 export GRADLE_HOME=$srcapp/gradle-3.5
-
-# go env
-export GOPATH=/Users/tyler/code/goWorkspaces
+export GOPATH=/Users/tyler/repository/goWorkspaces
 
 
-envArray=( $MAVEN_HOME/bin  $GRADLE_HOME/bin  )
+envArray=( 
+$MAVEN_HOME/bin  
+$GRADLE_HOME/bin  
+$GOPATH/bin  
+)
 for i in "${envArray[@]}"
 do
 	export PATH=$PATH:$i
